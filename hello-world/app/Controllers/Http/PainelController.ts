87aaefdef498c23d06c79983bc85956e624c9aea
wesleibruno <weslei945@gmail.com>
2021-12-20 23:07:32 -0300

    Passando e validando parametros[1]

--- hello-world/app/Controllers/Http/PainelController.ts
+++ hello-world/app/Controllers/Http/PainelController.ts
@@ -1,13 +1,34 @@
 // import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class PainelController {
+  protected users = [
+    {
+      id: 1,
+      name: "Weslei Bruno",
+    },
+    {
+      id: 2,
+      name: "Fulano",
+    },
+  ];
   async index() {
     return { response: "Index do Painel" };
   }
   async usuarios() {
-    return { response: "Usuários do painel" };
+    return {
+      user: this.users,
+    };
   }
-  async admin() {
-    return { response: "Roda admin" };
+
+  async usuario({ params }) {
+    let myRequestedUserId = params["id"];
+    let myUser = this.users.find((user) => user.id == myRequestedUserId);
+
+    if (myUser) {
+      return myUser;
+    } else {
+      return { error: "Nenhum usuario encontrado" };
+    }
+    return myUser;
   }
 }
--- hello-world/start/api.routes.ts
+++ hello-world/start/api.routes.ts
@@ -1,10 +1,9 @@
 import Route from "@ioc:Adonis/Core/Route";
 
 Route.group(() => {
-  Route.get("/admin/", "PainelController.admin");
-
   Route.group(() => {
     Route.get("/", "PainelController.index");
     Route.get("/usuarios", "PainelController.usuarios");
+    Route.get("/usuarios/:id", "PainelController.usuario");
   }).prefix("/painel/");
 }).prefix("/api");
